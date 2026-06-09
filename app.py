5c2706dd4ef0a3bdd36ec1f5bff296407ae28538
Verônica M. Palevicius <vemarx.dev@gmail.com>
2026-06-09 14:59:23 -0300

    chore: prepara aplicação para deploy

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -35,4 +36,5 @@ def relatorios():
     return "Página de relatórios do sistema"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
