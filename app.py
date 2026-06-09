6167091867ab6c453c11f54093aeeeae82a0f4bb
Verônica M. Palevicius <vemarx.dev@gmail.com>
2026-06-09 13:37:04 -0300

    Melhora descrição da rota de cadastro

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def contato():
 
 @app.route("/cadastro-livro")
 def cadastro_livro():
-    return "Página de cadastro de livros"
+    return "Formulário de cadastro de livros"
 
 if __name__ == "__main__":
     app.run(debug=True)
