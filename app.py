ed2a127a46e06a61d021f35a0249ad997db03a99
Verônica M. Palevicius <vemarx.dev@gmail.com>
2026-06-09 13:33:37 -0300

    Adiciona rota de listagem de livros

--- app.py
+++ app.py
@@ -10,5 +10,9 @@ def home():
 def sobre():
     return "Sistema desenvolvido em Flask para estudo de CI/CD"
 
+@app.route("/livros")
+def livros():
+    return "Lista de livros cadastrados"
+
 if __name__ == "__main__":
     app.run(debug=True)
