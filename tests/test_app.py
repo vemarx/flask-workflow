acb893da83e17b08940bfa4bbc1e96218e0e19bd
Verônica M. Palevicius <vemarx.dev@gmail.com>
2026-06-09 14:16:24 -0300

    test: adiciona testes da aplicação

---
+++ tests/test_app.py
@@ -0,0 +1,11 @@
+from app import app
+
+def test_home():
+    client = app.test_client()
+    response = client.get("/")
+    assert response.status_code == 200
+    
+def test_sobre():
+    client = app.test_client()
+    response = client.get("/sobre")
+    assert response.status_code == 200
